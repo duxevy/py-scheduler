d670629977461e6397c572c37a575561456100a8
duxevy <79513394+duxevy@users.noreply.github.com>
2021-11-13 01:13:38 +0300

    commit

--- todoist/app/urls.py
+++ todoist/app/urls.py
@@ -5,6 +5,6 @@ app_name = 'app'
 urlpatterns = [
     path('notes/', NoteView.as_view()),
     path('notes/<int:note_id>/', NoteDetailView.as_view()),
-    path('notes/add/', NoteEditorView.as_view()),
+    path('notes/editor/', NoteEditorView.as_view()),
     path('notes/delete/<int:note_id>/', NoteDeleteView.as_view()),
 ]
